3974fe5de6fe103c5b8a3b22818e46e86f384bcf
David Pinto <daxtery11@gmail.com>
2021-06-29 14:56:10 +0100

    change how search stores the results

--- src/app/core/services/equipment-details-service.ts
+++ src/app/core/services/equipment-details-service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { skip } from 'rxjs/operators';
 import { Equipment } from 'src/app/shared/types';
 import { EquipmentService } from './equipment.service';
 
@@ -8,34 +9,36 @@ import { EquipmentService } from './equipment.service';
 })
 export class EquipmentDetailsService {
 
+    private _selected: BehaviorSubject<Equipment | undefined>;
     selected: Observable<Equipment>;
 
-    private _selected: Subject<Equipment>;
-    private _lastId?: string;
-
     constructor(private equipmentService: EquipmentService) {
-        this._selected = new Subject();
-        this.selected = this._selected.asObservable();
+        this._selected = new BehaviorSubject<Equipment | undefined>(undefined);
+        this.selected = this._selected.pipe(skip(1));
     }
 
     set(equipment: Equipment): void;
     set(id: string): void;
 
     async set(id_or_equipment: string | Equipment) {
+        let equipment: Equipment;
+        const value = this._selected.value;
+
         if (typeof id_or_equipment === 'string') {
-            if (this._lastId != id_or_equipment) {
-                const equipment = ((await this.equipmentService.queryById(id_or_equipment).toPromise()).data)["queryById"];
+            if (value && id_or_equipment == value._id) {
+                equipment = value;
+            } else {
+                equipment = ((await this.equipmentService.queryById(id_or_equipment).toPromise()).data)["queryById"];
                 this._selected.next(equipment);
             }
 
-            this._lastId = id_or_equipment;
         } else {
-            if (id_or_equipment._id != this._lastId) {
-                this._selected.next(id_or_equipment);
+            equipment = id_or_equipment;
+            if (value && equipment._id != value._id) {
+                this._selected.next(equipment);
             }
-
-            this._lastId = id_or_equipment._id;
         }
+
     }
 
 }
