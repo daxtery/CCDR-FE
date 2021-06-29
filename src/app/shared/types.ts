7f3eec6b833871ce8e4052e626fa431f1c89ac5d
David Pinto <daxtery11@gmail.com>
2021-06-29 15:42:57 +0100

    add other types' typings?

--- src/app/shared/types.ts
+++ src/app/shared/types.ts
@@ -13,3 +13,36 @@ export interface EquipmentAndScore {
     equipment: Equipment;
     score: number;
 };
+
+export interface SocialDetails {
+    fins_lucrativos: boolean;
+};
+
+export interface CultureDetails {
+    acesso_gratuito: string;
+    num_visitantes_medio: number;
+    mobilidade_reduzida: boolean;
+    tutela: string;
+};
+
+export interface SportDetails {
+    iluminado: boolean;
+    tipo_piso: string;
+    mobilidade_reduzida_pratica: boolean;
+    mobilidade_reduzida_assistencia: boolean;
+};
+
+export interface HospitalHealthDetails {
+    agrupamento_saude: string;
+    centro_sospitalar: string;
+    valencias: string[];
+    especialidades: string[];
+};
+
+export interface SchoolDetails {
+    grau_ensino: string;
+};
+
+export interface EducationDetails {
+    escolas: SchoolDetails[];
+};
