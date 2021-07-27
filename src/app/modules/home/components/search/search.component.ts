a23998aa307a8b413e2c40e9ae270e34d915f7ed
SyNthw8ve <synthw8v3@gmail.com>
2021-07-27 13:36:00 +0100

    Search Dropdown

--- src/app/modules/home/components/search/search.component.ts
+++ src/app/modules/home/components/search/search.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { EquipmentService } from 'src/app/core/services/equipment.service';
+import { EquipmentPreview } from 'src/app/shared/types';
 
 @Component({
   selector: 'app-search',
@@ -12,11 +14,14 @@ export class SearchComponent implements OnInit {
 
   clickedMap = new Map<string, boolean>();
   scoresMap = new Map<string, number>();
+  lastSearch: Array<string>;
+  dropdownVisible: boolean = false;
 
   searchFormGroup: FormGroup;
 
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private equipmentService: EquipmentService,
   ) { }
 
   ngOnInit(): void {
@@ -26,36 +31,25 @@ export class SearchComponent implements OnInit {
       searchValue: ['']
     })
 
+    this.equipmentService.getLastNQueries(5).subscribe(({ data }) => {
+
+      this.lastSearch = data.lastNUniqueQueries;
+    })
+
+  }
+
+  onFocus() {
+
+    this.dropdownVisible = true;
+  }
+
+  onFocusOut() {
+
+    this.dropdownVisible = false;
   }
 
   search() {
     console.log("Goooo")
-    /* this.sendQueriesFeedback();
-
-    const searchValue = this.searchFormGroup.get('searchValue').value
-
-    if (searchValue != '') {
-
-      this.scoresMap.clear();
-      this.clickedMap.clear();
-      this.$queryResults = []
-
-      this.equipmentService.queryEquipments(searchValue).subscribe(({ data }) => {
-
-        data['queryEquipments'].forEach((value) => {
-          this.scoresMap.set(value.equipment._id, value.score);
-          this.clickedMap.set(value.equipment._id, false);
-          this.$queryResults.push(value.equipment);
-        })
-
-        this.previousSearch = searchValue;
-        this.searchFormGroup.get('searchValue').reset();
-
-      }, (error) => {
-        console.log('there was an error sending the query', error);
-      });
-
-    } */
 
   }
 }
